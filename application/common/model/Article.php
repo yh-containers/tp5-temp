ee93d030793ae279b314f695b577d9d919f2bda3
yehua <1104753531@qq.com>
2019-07-18 16:11:53 +0800

    add

---
+++ application/common/model/Article.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\common\model;
+
+//
+use think\model\concern\SoftDelete;
+
+class Article extends BaseModel
+{
+    use SoftDelete;
+    //数据库表名
+    protected $name = 'article';
+
+
+}
